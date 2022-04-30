adb3e71d2e6596bb72c856998b04dde7c245930c
Manuel Soto <79372727+manugram@users.noreply.github.com>
2022-04-30 02:49:41 -0400

    Rewrite for version 0.2.0
    Show some uses...

--- test.py
+++ test.py
@@ -8,30 +8,31 @@ db = DBooks()
 def main():
 
     # Query for recent uploaded books
-    r = db.recent_books()
+    r = db.get_recents_books()
 
     # Print the nomber of books return
     print(r.get('total'))
 
-    # Query for search term, gcc in this example
-    s = db.search_book('asm')
+    # Query for search term, python in this example
+    s = db.search_book('python')
 
     # Print total number of books found in query
     print('Total books:',s['total'])
+
+    # Show books title and url...
     for x in s['books']:
-        pp(x)
-        print('+-------------------------+')
+        print(x['title'])
+        print(x['url'])
+        print('+' + '-----'*20 + '+')
 
 
     # Pretty print specific book details, as a formatted string, losing
     # dictionaries advantages
-    d = db.book_details('5606038334', indent=2)
-    print(d)
-
-
-
+    d = db.get_book_details('1530051126')
+    pp(d)
 
 
 
 if __name__ == '__main__':
     main()
+
